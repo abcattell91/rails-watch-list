6cf3bb76e9e5710128aaa92fa3dea8da0a824f76
Alex Cattell <abcattell91@gmail.com>
2022-04-30 16:46:43 +0100

    Needs more styling work. Implemented javascript transition.

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,7 +8,10 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 import "bootstrap";
+import "controllers"
 
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+import "controllers"
